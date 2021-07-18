af826655d00b0cc58c349b396769861d68592a24
Sergey <tel.cromax@gmail.com>
2021-07-18 20:06:40 +1100

    add Mile App

---
+++ src/Mile.java
@@ -0,0 +1,11 @@
+public class Mile {
+    public static void main(String[] args) {
+        int ticketPrice = 7500;
+        int bonus = 20;
+        int mile;
+
+        mile = ticketPrice / bonus;
+
+        System.out.println(mile);
+    }
+}
